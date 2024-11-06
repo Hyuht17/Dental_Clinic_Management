c348f6e6a5ac71d2ed2329c69e4b64199aa4abf7
kiwwi1 <dquanghuy221@gmail.com>
2024-11-06 21:53:30 +0700

    header va footer

---
+++ FE/src/pages/homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import FooterComponent from "../../components/footer/FooterComponent";
+import Navbar from "../../components/navbar/Navbar";
+function Homepage(){
+    return(
+        <>
+          <Navbar/>
+          <FooterComponent/>
+        </>
+
+    );
+};
+
+export default Homepage
