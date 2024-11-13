3a2909600256744a10e1a22421296725166eabf7
kiwwi1 <dquanghuy221@gmail.com>
2024-11-13 23:08:38 +0700

    update fe test admin

--- FE/admin/src/pages/Admin/AddDoctor.jsx
+++ FE/admin/src/pages/Admin/AddDoctor.jsx
@@ -11,38 +11,26 @@ const AddDoctor = () => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [experience, setExperience] = useState('1 Year')
     const [fees, setFees] = useState('')
-    const [about, setAbout] = useState('')
     const [speciality, setSpeciality] = useState('General physician')
-    const [degree, setDegree] = useState('')
-    const [address1, setAddress1] = useState('')
-    const [address2, setAddress2] = useState('')
-
     const { backendUrl } = useContext(AppContext)
     const { aToken } = useContext(AdminContext)
 
     const onSubmitHandler = async (event) => {
         event.preventDefault()
-
         try {
-
             if (!docImg) {
                 return toast.error('Image Not Selected')
             }
-
             const formData = new FormData();
 
             formData.append('image', docImg)
             formData.append('name', name)
             formData.append('email', email)
             formData.append('password', password)
-            formData.append('experience', experience)
             formData.append('fees', Number(fees))
-            formData.append('about', about)
             formData.append('speciality', speciality)
-            formData.append('degree', degree)
-            formData.append('address', JSON.stringify({ line1: address1, line2: address2 }))
+            
 
             // console log formdata            
             formData.forEach((value, key) => {
@@ -56,10 +44,7 @@ const AddDoctor = () => {
                 setName('')
                 setPassword('')
                 setEmail('')
-                setAddress1('')
-                setAddress2('')
-                setDegree('')
-                setAbout('')
+            
                 setFees('')
             } else {
                 toast.error(data.message)
@@ -106,20 +91,7 @@ const AddDoctor = () => {
                             <input onChange={e => setPassword(e.target.value)} value={password} className='border rounded px-3 py-2' type="password" placeholder='Password' required />
                         </div>
 
-                        <div className='flex-1 flex flex-col gap-1'>
-                            <p>Experience</p>
-                            <select onChange={e => setExperience(e.target.value)} value={experience} className='border rounded px-2 py-2' >
-                                <option value="1 Year">1 Year</option>
-                                <option value="2 Year">2 Years</option>
-                                <option value="3 Year">3 Years</option>
-                                <option value="4 Year">4 Years</option>
-                                <option value="5 Year">5 Years</option>
-                                <option value="6 Year">6 Years</option>
-                                <option value="8 Year">8 Years</option>
-                                <option value="9 Year">9 Years</option>
-                                <option value="10 Year">10 Years</option>
-                            </select>
-                        </div>
+                        
 
                         <div className='flex-1 flex flex-col gap-1'>
                             <p>Fees</p>
@@ -143,25 +115,14 @@ const AddDoctor = () => {
                         </div>
 
 
-                        <div className='flex-1 flex flex-col gap-1'>
-                            <p>Degree</p>
-                            <input onChange={e => setDegree(e.target.value)} value={degree} className='border rounded px-3 py-2' type="text" placeholder='Degree' required />
-                        </div>
+                      
 
-                        <div className='flex-1 flex flex-col gap-1'>
-                            <p>Address</p>
-                            <input onChange={e => setAddress1(e.target.value)} value={address1} className='border rounded px-3 py-2' type="text" placeholder='Address 1' required />
-                            <input onChange={e => setAddress2(e.target.value)} value={address2} className='border rounded px-3 py-2' type="text" placeholder='Address 2' required />
-                        </div>
+                        
 
                     </div>
 
                 </div>
 
-                <div>
-                    <p className='mt-4 mb-2'>About Doctor</p>
-                    <textarea onChange={e => setAbout(e.target.value)} value={about} className='w-full px-4 pt-2 border rounded' rows={5} placeholder='write about doctor'></textarea>
-                </div>
 
                 <button type='submit' className='bg-primary px-10 py-3 mt-4 text-white rounded-full'>Add doctor</button>
 
